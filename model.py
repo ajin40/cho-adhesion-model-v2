152f72abeafd40030f00b4254599ea8411d6077f
Andrew Jin <ajin40@gatech.edu>
2022-09-27 23:17:47 -0400

    moved function call of abm to model file

--- model.py
+++ model.py
@@ -373,7 +373,44 @@ class TestSimulation(Simulation):
         name = backend.check_existing(name, output_dir, new_simulation=True)
         cls.simulation_mode_0(name, output_dir, model_params)
 
-
+def parameter_sweep_abm(par, directory, RR, YY, RY, dox_ratio, aba_ratio, final_ts=60):
+    """ Run model with specified parameters
+        :param par: simulation number
+        :param directory: Location of model outputs. A folder titled 'outputs' is required
+        :param RR: Adhesion value for R-R cell interactions
+        :param YY: Adhesion value for Y-Y cell interactions
+        :param RY: Adhesion value for R-Y cell interactions
+        :param dox_ratio: final ratio of Red cells at simulation end. 1 - (dox_ratio + aba_ratio) = # of remaining uncommitted blue cells.
+        :param aba_ratio: final ratio of Yellow cells at simulation end.
+        :param final_ts: Final timestep. 60 ts = 96h, 45 ts = 72h
+        :type par int
+        :type directory: String
+        :type RR: float
+        :type YY: float
+        :type RY: float
+        :type dox_ratio: float
+        :type aba_ratio: float
+        :type final_ts: int
+    """
+    model_params = {
+        "u_bb": 1,
+        "u_rb": 1,
+        "u_yb": 1,
+        "u_rr": RR,
+        "u_repulsion": 10000,
+        "alpha": 10,
+        "gravity": 2,
+        "end_step": final_ts,
+        "u_yy": YY,
+        "u_ry": RY,
+        "dox_ratio": dox_ratio,
+        "aba_ratio": aba_ratio,
+        "PACE": False
+    }
+    name = f'urr{RR}_uyy{YY}_ury{RY}_dox{dox_ratio}_aba{aba_ratio}'
+    sim = TestSimulation(model_params)
+    sim.start_sweep(directory + '/outputs', model_params, name)
+    return par, sim.image_quality, sim.image_quality, 3, final_ts/sim.sub_ts
 
 if __name__ == "__main__":
     model_params = {
@@ -390,5 +427,7 @@ if __name__ == "__main__":
         "aba_ratio": 0.3,
         "PACE": False
     }
-    sim = TestSimulation(model_params)
-    sim.start("/Users/andrew/PycharmProjects/ST_CHO_adhesion_model/outputs/", model_params)
+    a = parameter_sweep_abm(0, "/Users/andrew/PycharmProjects/ST_CHO_adhesion_model/", 20, 20, 1, 0.5, 0.2, final_ts=2)
+    print(a)
+    # sim = TestSimulation(model_params)
+    # sim.start("/Users/andrew/PycharmProjects/ST_CHO_adhesion_model/outputs/", model_params)
